c133b83bc5de3e160ed62f7e686d389895cb1236
Omari Domville <omaridom@hotmail.com>
2019-11-13 09:30:38 -0500

    Initial Commit

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users(
+        id INT PRIMARY KEY,
+        firstname CHAR(20),
+        lastname CHAR(20),
+        password CHAR(18),
+        email CHAR,
+        date_joined date
+);
+CREATE TABLE issues(
+        id INT PRIMARY KEY,
+        title CHAR,
+        description CHAR,
+        type CHAR(18),
+        priority CHAR,
+        status CHAR,
+        assign_to INT,
+        created_by INT,
+        created CHAR,
+        updated CHAR
+);
+        
